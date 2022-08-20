b4a126c6d8d29ae2b10326e25dd8d6eb3460c4f9
xiumubai <1547702880@qq.com>
2022-08-20 17:50:28 +0800

    style(ino): 个人信息展示

---
+++ src/components/v-panel/v-panel.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: 朽木白
+ * @Date: 2022-08-20 17:04:01
+ * @LastEditors: 1547702880@qq.com
+ * @LastEditTime: 2022-08-20 17:10:00
+ * @Description: 
+-->
+<template>
+  <view class="text"></view>
+</template>
+
+<script>
+export default {
+  name: 'VPanel',
+};
+</script>
+
+<style lang="scss" scoped>
+.text {
+  color: $price-font-color;
+}
+</style>
--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -2,15 +2,35 @@
  * @Author: 朽木白
  * @Date: 2022-08-19 11:10:54
  * @LastEditors: 1547702880@qq.com
- * @LastEditTime: 2022-08-20 09:46:21
+ * @LastEditTime: 2022-08-20 17:47:16
  * @Description: 
 -->
 <template>
   <view class="container">
-    <uni-list>
-      <uni-list-item title="第一行"></uni-list-item>
-      <uni-list-item title="第二行"></uni-list-item>
-    </uni-list>
+    <view class="info dark-mode">
+      <h1 class="info_title">
+        <image
+          class="info_title_logo"
+          src="https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJCzAG0lh2uzKeiad4V3Sj8dCIAeC1coAaGQoqQGZVD2clAubcEJIgUhu7uAogjHib4FsPZCl6IpsLA/132"
+        />
+        <text class="info_title_name">朽木白</text>
+      </h1>
+      <view class="info_list">
+        <view class="info_item">
+          <navigator class="item_a info-link">
+            <uni-icons type="shop-filled" size="24" color="#a1a7b2"></uni-icons>
+            <text class="item_dsc">订单管理</text>
+          </navigator>
+        </view>
+        <view class="info_item">
+          <navigator class="item_a info-link">
+            <uni-icons type="vip-filled" size="24" color="#a1a7b2"></uni-icons>
+            <text class="item_dsc">我的学习</text>
+          </navigator>
+        </view>
+      </view>
+    </view>
+    <view class="list"></view>
   </view>
 </template>
 
@@ -28,4 +48,39 @@ export default {
 
 <style lang="scss">
 @import url('@/static/styles/_global.scss');
+.info {
+  &_title {
+    height: 97px;
+    padding: 0 24px;
+    display: flex;
+    align-items: center;
+    &_logo {
+      height: 60px;
+      width: 60px;
+      border-radius: 50%;
+    }
+    &_name {
+      margin-left: 10px;
+      color: #c0c4cc;
+      font-size: 20px;
+    }
+  }
+  &_list {
+    display: flex;
+    .info_item {
+      width: 50%;
+      text-align: center;
+      .item_a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 24px 0;
+      }
+      .item_dsc {
+        font-size: 14px;
+        margin-top: 8px;
+      }
+    }
+  }
+}
 </style>
