ef6476c63c07094cdc6bdaab51a71eb36d87bcce
xiumubai <1547702880@qq.com>
2022-08-22 08:33:01 +0800

    feat(login): add login page

---
+++ src/pages/login/index.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: 朽木白
+ * @Date: 2022-08-20 18:17:29
+ * @LastEditors: 1547702880@qq.com
+ * @LastEditTime: 2022-08-22 08:32:42
+ * @Description: 
+-->
+<template>
+  <view class="container"></view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import url('@/static/styles/_global.scss');
+</style>
