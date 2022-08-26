6d3d21c4c2c851ada35485f0fdcc3055588b75b4
xiumubai <1547702880@qq.com>
2022-08-26 10:59:00 +0800

    feat(comment): 上拉加载数据

--- src/pages/course/comment.vue
+++ src/pages/course/comment.vue
@@ -2,12 +2,16 @@
  * @Author: 朽木白
  * @Date: 2022-08-26 10:42:43
  * @LastEditors: 1547702880@qq.com
- * @LastEditTime: 2022-08-26 10:53:11
+ * @LastEditTime: 2022-08-26 10:57:47
  * @Description: 
 -->
 <template>
   <view class="container comment">
     <v-comment :list="commentList"></v-comment>
+    <!-- 加载更多 -->
+    <view class="load_more">
+      <uni-load-more :status="status" />
+    </view>
   </view>
 </template>
 
@@ -17,19 +21,35 @@ export default {
   data() {
     return {
       commentList: [],
+      params: {
+        page: 1,
+        limit: 10,
+      },
+      status: 'more',
     };
   },
   onLoad() {
     this.getCommeentList();
   },
+  // 监听滚动事件最外层view不能添加css样式overflow：scroll
+  onReachBottom() {
+    if (this.status !== 'noMore') {
+      this.status = 'loading';
+      this.params.page++;
+      this.getCommeentList();
+    }
+  },
   methods: {
     async getCommeentList() {
       try {
         const res = await userService.commentList({
-          page: 1,
-          limit: 10,
+          ...this.params,
         });
-        this.commentList = res.data.items;
+        const items = res.data.items;
+        // 数组解构拼接
+        this.commentList = [...this.commentList, ...items];
+        if (items.length < 10) return (this.status = 'noMore');
+        if (items.length >= 10) this.status = 'more';
         console.log('res', res);
       } catch (e) {
         console.log('e', e);
