e0913a811f649628419a898d6714aed096de6403
xiumubai <1547702880@qq.com>
2022-09-17 11:13:19 +0800

    fix: 解决输入框被软键盘遮挡的问题

--- src/pages/course/comment.vue
+++ src/pages/course/comment.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: 朽木白
  * @Date: 2022-08-26 10:42:43
- * @LastEditors: 1547702880@qq.com
- * @LastEditTime: 2022-08-27 18:00:33
+ * @LastEditors: xxx@xxx.com
+ * @LastEditTime: 2022-09-17 11:11:11
  * @Description: 评论列表
 -->
 <template>
@@ -13,27 +13,29 @@
       <uni-load-more :status="status" />
     </view>
 
-    <!-- <uni-easyinput
-      type="textarea"
-      autoHeight
-      v-model="content"
-      placeholder="请输入内容"
-    ></uni-easyinput> -->
-
     <view class="comment_bar">
-      <uni-search-bar
-        @confirm="handleComment"
-        placeholder="点击这里可以添加评论"
+      <input
+        class="input_box"
+        placeholder="请点击输入"
+        cursor-spacing="10"
         v-model="content"
-        bgColor="#EEEEEE"
-      >
-      </uni-search-bar>
+        @confirm="handleComment"
+        @focus="handleFocus"
+        @blur="handleBlur"
+        @input="handleInput"
+      />
+      <uni-icons
+        @click="handleComment"
+        class="input_icon"
+        type="paperplane-filled"
+        size="20"
+      />
     </view>
   </view>
 </template>
 
 <script>
-import userService from '@/services/user';
+import userService from "@/services/user";
 export default {
   data() {
     return {
@@ -43,8 +45,9 @@ export default {
         limit: 10,
       },
       options: {},
-      status: 'more',
-      content: '',
+      status: "more",
+      content: "",
+      isInput: false,
     };
   },
   onLoad(option) {
@@ -53,8 +56,8 @@ export default {
   },
   // 监听滚动事件最外层view不能添加css样式overflow：scroll
   onReachBottom() {
-    if (this.status !== 'noMore') {
-      this.status = 'loading';
+    if (this.status !== "noMore") {
+      this.status = "loading";
       this.params.page++;
       this.getCommentList();
     }
@@ -69,11 +72,11 @@ export default {
         const items = res.data.items;
         // 数组解构拼接
         this.commentList = [...this.commentList, ...items];
-        if (items.length < 10) return (this.status = 'noMore');
-        if (items.length >= 10) this.status = 'more';
-        console.log('res', res);
+        if (items.length < 10) return (this.status = "noMore");
+        if (items.length >= 10) this.status = "more";
+        console.log("res", res);
       } catch (e) {
-        console.log('e', e);
+        console.log("e", e);
       }
     },
     /**
@@ -89,35 +92,44 @@ export default {
           },
         });
 
-        if (res.message == '成功') {
+        if (res.message == "成功") {
           uni.showToast({
-            title: '评论发送成功',
+            title: "评论发送成功",
           });
-          this.content = '';
+          this.content = "";
         } else {
           uni.showToast({
-            title: '评论发送失败',
-            icon: 'error',
+            title: "评论发送失败",
+            icon: "error",
           });
         }
       } catch (e) {
-        console.log('e', e);
+        console.log("e", e);
       }
     },
     handleComment() {
-      this.$store.dispatch('goLogin', () => {
+      this.$store.dispatch("goLogin", () => {
         this.commentAdd();
       });
+    },
+    handleFocus() {
+      this.isInput = true;
+    },
+    handleBlur() {
+      this.isInput = false;
+    },
+    handleInput() {
+      this.isInput = true;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-@import url('@/static/styles/_global.scss');
+@import url("@/static/styles/_global.scss");
 .comment {
   background: white;
-  padding: 16px 16px 76px 16px;
+  padding: 16px 16px 50px 16px;
   height: 100%;
   .comment_bar {
     position: fixed;
@@ -125,7 +137,10 @@ export default {
     left: 0;
     width: 100%;
     z-index: 999;
-    height: 76px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     background: linear-gradient(
       180deg,
       hsla(0, 0%, 86.7%, 0) 0,
@@ -134,6 +149,20 @@ export default {
       #fff 0.64286rem,
       #fff
     );
+
+    .input_box {
+      width: 80%;
+      height: 30px;
+      line-height: 30px;
+      background-color: #f2f3f7;
+      border-radius: 20px;
+      // text-indent: 10px;
+      padding-left: 10px;
+      font-size: 16px;
+    }
+    .input_icon {
+      margin-left: 8px;
+    }
   }
 }
 </style>
